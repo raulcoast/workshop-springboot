8327bdbd99cfa0dbc9ab2969e7a7fbb7a61809f6
Raul Costa <raulcoast@gmail.com>
2023-04-18 21:58:59 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/raulcoast/demo/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.raulcoast.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raulcoast.demo.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>  {
+
+}
---
+++ src/main/java/com/raulcoast/demo/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.raulcoast.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raulcoast.demo.entities.Order;
+import com.raulcoast.demo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
